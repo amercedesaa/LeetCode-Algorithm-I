b91a5a463717d9dae947d2151db14e481be6df21
amercedesaa <90951960+amercedesaa@users.noreply.github.com>
2021-11-09 11:35:50 -0600

    Create 167-Two-Sum-II-Input-Array-Is-Sorted.py

---
+++ 167-Two-Sum-II-Input-Array-Is-Sorted.py
@@ -0,0 +1,12 @@
+class Solution:
+    def twoSum(self, numbers: List[int], target: int) -> List[int]:
+        a=0
+        b=len(numbers)-1
+        while a<b:
+            s=numbers[a]+numbers[b]
+            if s<target:
+                a+=1
+            elif s==target:
+                return [a+1,b+1]
+            elif s>target:
+                b-=1
